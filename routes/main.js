dc559ae8d91d7fbd49bbe29e5845943fce1ba66b
Fernan de Dios <info@mediagramstudios.com>
2018-01-31 11:21:03 +0800

    added courses/:id route

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,5 @@
 const Course = require('../models/course');
+const User = require('../models/user');
 
 module.exports = (app) => {
   app.get('/', (req, res, next) => {
@@ -8,6 +9,62 @@ module.exports = (app) => {
   app.get('/courses', (req, res, next) => {
     Course.find({}, (err, courses) => {
       res.render('courses/courses', { courses });
+    });
+  });
+
+  app.get('/courses/:id', (req, res, next) => {
+    async.parallel([
+      (callback) => {
+        Course.findOne({ _id: req.params.id }, (err, foundCourse) => {
+          callback(err, foundCourse);
+        });
+      },
+
+      (callback) => {
+        // see if user has already taken the course
+        User.findOne({ _id: req.user.id, 'coursesTaken.course': req.params.id })
+          // fetch details of course
+          .populate('coursesTaken.course')
+          .exec((err, foundUserCourse) => {
+            callback(err, foundUserCourse);
+          });
+      },
+
+      (callback) => {
+        // see if user made the course
+        User.findOne({ _id: req.user.id, 'coursesTeach.course': req.params.id })
+          // fetch details of course
+          .populate('coursesTeach.course')
+          .exec((err, foundUserCourse) => {
+            callback(err, foundUserCourse);
+          });
+      }
+
+    ], (err, results) => {
+      // from first fn
+      const course = results[0];
+
+      // from 2nd fn
+      const userCourse = results[1];
+
+      // from 3rd fn
+      const teacherCourse = results[2];
+
+      // user is not taking course, and is not the teacher
+      if (userCourse === null && teacherCourse === null) {
+        res.render('courses/courseDesc', { course });
+      }
+
+      // user is the teacher of the course
+      else if (userCourse === null && teacherCourse !== null) {
+        res.render('courses/course', { course });
+      }
+
+      // user is taking the course
+      else {
+        res.render('courses/course', { course });
+      }
+
     });
   });
 };
