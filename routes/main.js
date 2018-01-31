299ebe4e8a590fc96b5e9cec7393cc30abb0f516
Fernan de Dios <info@mediagramstudios.com>
2018-01-31 10:07:36 +0800

    added courses route

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,13 @@
+const Course = require('../models/course');
+
 module.exports = (app) => {
   app.get('/', (req, res, next) => {
     res.render('main/home');
+  });
+
+  app.get('/courses', (req, res, next) => {
+    Course.find({}, (err, courses) => {
+      res.render('courses/courses', { courses });
+    });
   });
 };
