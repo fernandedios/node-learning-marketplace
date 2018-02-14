1d7434987be01937f5ae57fc2f36b725c7f088b0
Fernan de Dios <info@mediagramstudios.com>
2018-02-14 14:38:09 +0800

    added route profile/edit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const passportConf = require('../middlewares/passport');
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = (app) => {
   app.get('/login', (req, res, next) => {
@@ -22,4 +23,23 @@ module.exports = (app) => {
     res.render('accounts/profile', { message: req.flash('loginMessage') }); //receive loginMessage
   });
 
+  app.route('/profile/edit')
+    .get(requireLogin, (req, res, next) => {
+      res.render('accounts/profile-edit', { user: req.user });
+    })
+    .post(requireLogin, async (req, res, next) => {
+      try {
+        const foundUser = await User.findOne({ _id: req.user.id });
+
+        if (req.body.name) foundUser.profile.name = req.body.name;
+        if (req.body.title) foundUser.profile.title = req.body.title;
+        if (req.body.picture) foundUser.profile.picture = req.body.picture;
+        if (req.body.desc) foundUser.profile.desc = req.body.desc;
+
+        await foundCourse.save();
+      }
+      catch (err) {
+        return next(err);
+      }
+    });
 };
